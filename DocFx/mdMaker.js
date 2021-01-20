73b37500c419bfe4029100117f8ee81592c88c20
Kiarash Tamaddon <ktamaddon@yahoo.com>
2021-01-20 13:05:46 +0100

    Update mdMaker.js

--- DocFx/mdMaker.js
+++ DocFx/mdMaker.js
@@ -39,7 +39,19 @@ var turndownPluginGfm = require('turndown-plugin-gfm')
 
 var gfm = turndownPluginGfm.gfm
 var turndownService = new TurndownService()
+
+// Import plugins from turndown-plugin-gfm
+var turndownPluginGfm = require('turndown-plugin-gfm')
+var gfm = turndownPluginGfm.gfm
+var tables = turndownPluginGfm.tables
+var strikethrough = turndownPluginGfm.strikethrough
+
+
+// Use the gfm plugin
 turndownService.use(gfm)
+
+// Use the table and strikethrough plugins only
+turndownService.use([tables, strikethrough])
 
 // Reads the HTML files and converts them to Markdown and stores them.
 var localPath = docFxPath.concat('/_site/api/');
