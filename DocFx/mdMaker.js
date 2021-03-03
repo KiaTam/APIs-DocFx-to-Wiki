0ec768686e646c3495ed50e10f6fed2fb351b5c2
Kiarash Tamaddon <ktamaddon@yahoo.com>
2021-03-03 17:01:06 +0100

    Update mdMaker.js

--- DocFx/mdMaker.js
+++ DocFx/mdMaker.js
@@ -222,7 +222,7 @@ for(var i=0;i<fileNames.length;i++)
 			var tmp = fileNames[i];
 			inputFilePath =inputFilePath.concat(tmp);
 			mdFilaData= fs.readFileSync(inputFilePath, 'utf8');
-			mdFilaData = mdFilaData.replaceAll('(images/', '(https://github.com/KiaTam/APIs-DocFx-to-Wiki/blob/master/Documentation/Development/images/')
+			mdFilaData = mdFilaData.replace('(images/', '(https://github.com/KiaTam/APIs-DocFx-to-Wiki/blob/master/Documentation/Development/images/')
 			var outputFilePath = docFxPath.concat('/');
 			var outputFilePath = outputFilePath.concat(fileNames[i]);
 			console.log(outputFilePath);
