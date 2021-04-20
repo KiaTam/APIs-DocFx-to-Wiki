e354c301da874443b5a369dbba25345a37bb8def
Kiarash Tamaddon <ktamaddon@yahoo.com>
2021-04-20 16:42:00 +0200

    made classes public

---
+++ src/SampleDotnetProject/MyConcreteClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDotnetProject
+{
+    public class MyConcreteClass : MyInterface
+    {
+        public string Name { get; set; }
+    }
+}
